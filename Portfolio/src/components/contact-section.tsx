9b8d530de2024d06b74b5050fc80811e75bf02c6
JohnNasitem <jnasitem1@nait.ca>
2025-04-07 12:32:26 -0600

    added contact form

--- Portfolio/src/components/contact-section.tsx
+++ Portfolio/src/components/contact-section.tsx
@@ -1,14 +1,28 @@
 "use client"
 
 import useAnimateEntrance from './animate-entrance';
+import ContactForm from './contact-form';
 
 const ContactSection = () => {
     const section1Visible = useAnimateEntrance("contact_section1");
+    const section2Visible = useAnimateEntrance("contact_section2");
+    const section3Visible = useAnimateEntrance("contact_section3");
+
     return (
         <div id='contact' className="pageSection font-[family-name:var(--font-geist-mono)] bg-orange-500">
-            <div className="grid grid-rows-[1fr_1fr_1fr] p-8 sm:p-10">
-                <div id='contact_section1' className={`${section1Visible ? 'animate-fade-slide-in' : ''}`}>
-                    Temp
+            <div className="p-8 sm:p-10">
+                <h2 id='contact_section1' className={`text-2xl sm:text-6xl font-bold justify-self-left ${section1Visible ? 'animate-fade-slide-in' : ''}`}>
+                    Contact
+                </h2>
+                <div className={`bg-blue-300 h-1 w-20 mt-5 mb-5 ${section1Visible ? 'animate-fade-slide-in' : ''}`}/>
+
+                <div className='grid sm:grid-cols-2 gap-y-5 gap-x-20'>
+                    <div id='contact_section2' className={`${section2Visible ? 'animate-fade-slide-in' : ''} flex justify-center items-center w-full h-[600px]`}>
+                        Socials here
+                    </div>
+                    <div id='contact_section3' className={`${section3Visible ? 'animate-fade-slide-in' : ''} flex justify-center items-center w-full h-[600px]`}>
+                        <ContactForm />
+                    </div>
                 </div>
             </div> 
         </div>
