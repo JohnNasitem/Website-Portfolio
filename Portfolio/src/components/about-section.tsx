e10a9ead5b2f31ed6ec0688c705d83756ee87e72
JohnNasitem <jnasitem1@nait.ca>
2025-04-06 23:02:48 -0600

    finished about section contents

--- Portfolio/src/components/about-section.tsx
+++ Portfolio/src/components/about-section.tsx
@@ -4,20 +4,20 @@ import useAnimateEntrance from './animate-entrance';
 
 const AboutSection = () => {
     const section1Visible = useAnimateEntrance("about_section1");
+    const section2Visible = useAnimateEntrance("about_section2");
 
     return (
         <div id='about' className="pageSection font-[family-name:var(--font-geist-mono)] bg-red-500">
-            <div className="grid grid-rows-[1fr_1fr_1fr] p-8 sm:p-10">
-                <div id='about_section1' className={`${section1Visible ? 'animate-fade-slide-in' : ''}`}>
-                    <h2 className="text-2xl sm:text-6xl font-bold justify-self-left">
-                        About
-                    </h2>
-                    <div className='bg-blue-300 h-1 w-20 mt-5 mb-5'/>
-                    <p className="text-2x1 text-left sm:text-left">
-                        I am a Computer Engineering Techonlogist.
-                    </p>
-                </div>
-            </div> 
+            <div className="p-8 sm:p-10">
+                <h2 id="about_section1" className={`text-2xl sm:text-6xl font-bold text-left ${section1Visible ? 'animate-fade-slide-in' : ''}`}>
+                    About
+                </h2>
+                <div className={`bg-blue-300 h-1 w-20 mt-5 mb-5 ${section1Visible ? 'animate-fade-slide-in' : ''}`}/>
+                <p id="about_section2" className={`text-2x1 text-left ${section2Visible ? 'animate-fade-slide-in' : ''}`}>
+                    Hi, I'm John, a computer engineering techonlogist. I have a passion for technology and a strong desire to learn and grow in the field.
+                    I am interestd in software and game development, and i enjoy working on projects that challenge me to think critically and creatively.
+                </p>
+            </div>
         </div>
     )
 };
