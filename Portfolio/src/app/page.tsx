15cfa2a82d5fbe52df9eeea87b10fa4481f2caf6
JohnNasitem <jnasitem1@nait.ca>
2025-04-06 23:03:05 -0600

    added skills section

--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Navbar from "@/components/navbar";
 import HomeSection from "@/components/home-section";
 import AboutSection from "@/components/about-section"; 
+import SkillsSection from "@/components/skills-section";
 import ProjectsSection from "@/components/projects-section";
 import ContactSection from "@/components/contact-section";
 import ToTop from "@/components/to-top";
@@ -10,6 +11,7 @@ export default function Home() {
   const narbarLinks = [
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
     { name: "Contact", href: "#contact" },
   ]
@@ -20,6 +22,7 @@ export default function Home() {
       <Navbar links={narbarLinks} logo="grad.png" />
       <HomeSection />
       <AboutSection />
+      <SkillsSection />
       <ProjectsSection />
       <ContactSection />
     </div>
