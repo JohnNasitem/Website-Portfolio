d287c6942e3770f37ffff03ab95d5a81b7168946
JohnNasitem <jnasitem1@nait.ca>
2025-04-07 19:01:57 -0600

    added footer

--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -1,11 +1,11 @@
-import Navbar from "@/components/navbar";
-import ThemeToggle from "@/components/theme-toggle";
 import HomeSection from "@/components/home-section";
 import AboutSection from "@/components/about-section"; 
 import SkillsSection from "@/components/skills-section";
 import ProjectsSection from "@/components/projects-section";
 import ContactSection from "@/components/contact-section";
 import ToTop from "@/components/to-top";
+import Navbar from "@/components/navbar";
+import Link from "next/link";
 
 export default function Home() {
   const narbarLinks = [
@@ -20,14 +20,17 @@ export default function Home() {
     <div className="text-[var(--color-foreground)] bg-[var(--color-background)]">
       <ToTop />
       <Navbar links={narbarLinks} logo="grad.png" />
-      <div className="sticky top-0 right-0 w-fit bg-[var(--color-bg-alt-accent)] p-4 z-999 m-4">
-        <ThemeToggle/>
-      </div>
       <HomeSection />
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
       <ContactSection />
+      <div>
+        <div id='page_section1' className="text-center text-sm mt-4">
+          © 2025 John Nasitem. All rights reserved.<br/>
+          <Link target="_blank" href="https://icons8.com/icon/83221/sun" className="underline">Sun</Link> and <Link target="_blank" href="https://icons8.com/icon/59841/moon-symbol" className="underline">Moon</Link>  icon by <a target="_blank" href="https://icons8.com" className="underline">Icons8</a>
+        </div>
+      </div>
     </div>
   );
 }
