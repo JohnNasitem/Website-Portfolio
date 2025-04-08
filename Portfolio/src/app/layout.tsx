b1c6abc45ea9af6a33d467319ad3153e4cc11b41
JohnNasitem <jnasitem1@nait.ca>
2025-04-08 12:20:59 -0600

    made footer always be at the bottom and fixed nav bar links

--- Portfolio/src/app/layout.tsx
+++ Portfolio/src/app/layout.tsx
@@ -24,11 +24,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   const narbarLinks = [
-    { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
-    { name: "Skills", href: "#skills" },
-    { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
+    { name: "Home", href: "/#home" },
+    { name: "About", href: "/#about" },
+    { name: "Skills", href: "/#skills" },
+    { name: "Projects", href: "/#projects" },
+    { name: "Contact", href: "/#contact" },
   ]
 
   return (
@@ -36,15 +36,17 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="text-[var(--color-foreground)] bg-[var(--color-background)]">
+        <div className="text-[var(--color-foreground)] bg-[var(--color-background)] flex flex-col min-h-screen">
           <ToTop />
           <Navbar links={narbarLinks} logo="JN.svg" />
-          {children}
-          <div>
-          <div id='page_section1' className="text-center text-sm mt-4">
-            © 2025 John Nasitem. All rights reserved.
+          <div className="flex-1">
+            {children}
           </div>
-        </div>
+          <footer>
+            <div id='page_section1' className="text-center text-sm mt-4">
+              © 2025 John Nasitem. All rights reserved.
+            </div>
+          </footer>
         </div>
       </body>
     </html>
