078396c48eb404a006a594f17d34a92e118f6d43
JohnNasitem <jnasitem1@nait.ca>
2025-04-08 12:10:54 -0600

    added a projects page to display the project info

---
+++ Portfolio/src/app/projects.tsx
@@ -0,0 +1,16 @@
+export default function Projects() {
+    const narbarLinks = [
+      { name: "Home", href: "#home" },
+      { name: "About", href: "#about" },
+      { name: "Skills", href: "#skills" },
+      { name: "Projects", href: "#projects" },
+      { name: "Contact", href: "#contact" },
+    ]
+  
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+  
