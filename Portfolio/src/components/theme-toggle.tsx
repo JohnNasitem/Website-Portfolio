71831034eaf04fff8ca9814c31efab9d8eddc875
JohnNasitem <jnasitem1@nait.ca>
2025-04-07 19:00:50 -0600

    finished them toggle

--- Portfolio/src/components/theme-toggle.tsx
+++ Portfolio/src/components/theme-toggle.tsx
@@ -1,20 +1,24 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-
+import Image from 'next/image';
 
 const ThemeToggle = () => {
-    const [theme, setTheme] = useState(() => {
-        // Get the saved theme from localStorage or default to 'light'
-        return window.localStorage.getItem('theme') || 'light';
-    });
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null;
+        if (storedTheme) {
+            setTheme(storedTheme)
+        }
+    }, [])
 
     useEffect(() => {
         // Set theme
         document.documentElement.setAttribute('data-theme', theme);
 
         // save to local storage
-        window.localStorage.setItem('theme', theme);
+        localStorage.setItem('theme', theme);
     }, [theme]) // Re-run whenever theme changages
 
     const toggleTheme = () => {
@@ -23,8 +27,8 @@ const ThemeToggle = () => {
 
       
     return (
-        <button onClick={toggleTheme}>
-            switch to {theme === 'light' ? 'dark' : 'light'} mode
+        <button onClick={toggleTheme} className='hover:animate-grow animate-shrink'>
+            <Image src={`/${(theme === 'light' ? 'moon' : 'sun')}.png`} width={24} height={24} alt={`${(theme === 'light' ? 'moon' : 'sun')}`}/>
         </button>
     )
 }
