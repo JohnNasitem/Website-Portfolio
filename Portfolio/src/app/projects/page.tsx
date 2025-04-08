407effbd2970a5cfdd11216c1e137d0bda4d584b
JohnNasitem <jnasitem1@nait.ca>
2025-04-08 13:08:24 -0600

    added some types and components for the project info

--- Portfolio/src/app/projects/page.tsx
+++ Portfolio/src/app/projects/page.tsx
@@ -1,8 +1,39 @@
+import { Slabo_13px } from "next/font/google";
+
+enum SkillCatgegory {
+    Language = '#FF0000',
+    Framework = '#00FF00',
+    Other = '#0000FF'
+}
+
+type Skill = {
+    name: string
+    category: SkillCatgegory;
+}
+
+type ProjectInfoProps = {
+    name: string;
+    description: string;
+    skillsUsed: Skill[];
+}
+
+const ProjectInfo: React.FC<ProjectInfoProps> = ({ name, description, skillsUsed}) => {
+    return (
+        <div>
+            {name} - {description} -
+            {skillsUsed.map((skill, index) => (
+                <div key={index} style={{ backgroundColor: skill.category}}>{skill.name}</div>
+            ))}
+        </div>
+    );
+}
+
 export default function Home() {
     return (
        <div>
-         Hi
+            Add tags for different category of skills (ones displayed in the skills tab) color them by that category, each project should have a group of skills they use, and let the projects be sortable by skills
+            <ProjectInfo name='Test Project' description="Project Description" skillsUsed={[{name:'C#', category:SkillCatgegory.Language}]} />
        </div>
      );
-   }
+}
      
