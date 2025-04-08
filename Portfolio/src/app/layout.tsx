a2203b7e8c8ffa76c70736e6adc40deec4e6f451
JohnNasitem <jnasitem1@nait.ca>
2025-04-08 12:10:02 -0600

    moved nav bar, to top, and footer to layout

--- Portfolio/src/app/layout.tsx
+++ Portfolio/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ToTop from "@/components/to-top";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,17 +22,30 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  const narbarLinks = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+  ]
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="text-[var(--color-foreground)] bg-[var(--color-background)]">
+          <ToTop />
+          <Navbar links={narbarLinks} logo="JN.svg" />
+          {children}
+          <div>
+          <div id='page_section1' className="text-center text-sm mt-4">
+            © 2025 John Nasitem. All rights reserved.
+          </div>
+        </div>
+        </div>
       </body>
     </html>
   );
--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -3,32 +3,15 @@ import AboutSection from "@/components/about-section";
 import SkillsSection from "@/components/skills-section";
 import ProjectsSection from "@/components/projects-section";
 import ContactSection from "@/components/contact-section";
-import ToTop from "@/components/to-top";
-import Navbar from "@/components/navbar";
 
 export default function Home() {
-  const narbarLinks = [
-    { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
-    { name: "Skills", href: "#skills" },
-    { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
-  ]
-
   return (
-    <div className="text-[var(--color-foreground)] bg-[var(--color-background)]">
-      <ToTop />
-      <Navbar links={narbarLinks} logo="JN.svg" />
+    <div>
       <HomeSection />
       <AboutSection />
       <SkillsSection />
       <ProjectsSection />
       <ContactSection />
-      <div>
-        <div id='page_section1' className="text-center text-sm mt-4">
-          © 2025 John Nasitem. All rights reserved.
-        </div>
-      </div>
     </div>
   );
 }
