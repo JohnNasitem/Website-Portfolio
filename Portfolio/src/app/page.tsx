050be2096c4e812ff9fde7e92b1ddf7e804b66a4
JohnNasitem <jnasitem1@nait.ca>
2025-04-07 22:51:43 -0600

    removed attribution to unused icon

--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -5,7 +5,6 @@ import ProjectsSection from "@/components/projects-section";
 import ContactSection from "@/components/contact-section";
 import ToTop from "@/components/to-top";
 import Navbar from "@/components/navbar";
-import Link from "next/link";
 
 export default function Home() {
   const narbarLinks = [
@@ -27,8 +26,7 @@ export default function Home() {
       <ContactSection />
       <div>
         <div id='page_section1' className="text-center text-sm mt-4">
-          © 2025 John Nasitem. All rights reserved.<br/>
-          <Link target="_blank" href="https://icons8.com/icon/83221/sun" className="underline">Sun</Link> and <Link target="_blank" href="https://icons8.com/icon/59841/moon-symbol" className="underline">Moon</Link>  icon by <a target="_blank" href="https://icons8.com" className="underline">Icons8</a>
+          © 2025 John Nasitem. All rights reserved.
         </div>
       </div>
     </div>
