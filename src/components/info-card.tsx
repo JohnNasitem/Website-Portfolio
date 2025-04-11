53580aaa0bdff734da6fc2e3076efcb3156b6ed6
JohnNasitem <jnasitem1@nait.ca>
2025-04-10 22:28:35 -0600

    removed link property and used title instead

--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const InfoCard = ({ title, description, image, link }: {title: string, description: string, image: string, link: string}) => (
-    <Link href={link} className={`grid grid-rows-[auto_1fr] rounded-lg bg-[var(--color-bg-accent)] w-[75vw] sm:w-[400px] h-auto hover:shadow-[0_0px_15px_var(--color-foreground)] hover:animate-lift-up animate-lift-down`}>
+const InfoCard = ({ title, description, image }: {title: string, description: string, image: string}) => (
+    <Link href={'/projects?filter=' + title.replaceAll(' ', '%20')} className={`grid grid-rows-[auto_1fr] rounded-lg bg-[var(--color-bg-accent)] w-[75vw] sm:w-[400px] h-auto hover:shadow-[0_0px_15px_var(--color-foreground)] hover:animate-lift-up animate-lift-down`}>
         <Image className="rounded-t-lg w-full h-full" src={image} alt="Image" width={1000} height={1000}/>
         <div className='p-2'>
             <div className="font-bold text-2xl">{title}</div>
