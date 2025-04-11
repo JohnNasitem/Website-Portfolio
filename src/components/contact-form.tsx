d9617f2ab8068ce24b20932bd83654e3485036ef
JohnNasitem <jnasitem1@nait.ca>
2025-04-11 01:30:06 -0600

    fixed input name

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -13,7 +13,7 @@ const ContactForm = () => {
         if (!formRef.current) return;
 
         // Check if all fields are filled
-        if (!formRef.current.username.value || !formRef.current.email.value || !formRef.current.subject.value || !formRef.current.message.value) {
+        if (!formRef.current.mailername.value || !formRef.current.email.value || !formRef.current.subject.value || !formRef.current.message.value) {
             alert('All fields are required!');
             return;
         }
@@ -26,7 +26,9 @@ const ContactForm = () => {
             alert('Failed to send message.');
             return
         }
-        
+
+        console.log(formRef.current)
+
         emailjs.sendForm(serviceId, templateId, formRef.current, publicKey)
         .then(() => {
             alert('Message sent!');
@@ -42,7 +44,7 @@ const ContactForm = () => {
         <form ref={formRef} onSubmit={handleSubmit} className='rounded-lg bg-[var(--color-bg-accent)] p-4 sm:p-8 grid grid-cols-2 grid-rows-[auto_auto_auto_auto_1fr_auto] sm:grid-rows-[auto_auto_auto_1fr_auto] gap-x-10 gap-y-5 w-full h-full'>
             <label className='col-span-2 text-center text-2x1 sm:text-3xl flex m-x-auto justify-center text-[var(--color-foreground)]'>Send me an Email</label>
             <input type='hidden' name='time' value={`${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`}/>
-            <input type='text' name="username" placeholder="Your Name" className="p-2 bg-[var(--color-background)] rounded-lg w-full col-span-2 sm:col-span-1"/>
+            <input type='text' name="mailername" placeholder="Your Name" className="p-2 bg-[var(--color-background)] rounded-lg w-full col-span-2 sm:col-span-1"/>
             <input type='text' name="email" placeholder="Your Email" className="p-2 bg-[var(--color-background)] rounded-lg w-full col-span-2 sm:col-span-1"/>
             <input type='text' name="subject" placeholder="Email Subject" className="p-2 bg-[var(--color-background)] rounded-lg col-span-2 w-full"/>
             <textarea name='message' placeholder='Email Contents' className="p-2 bg-[var(--color-background)] rounded-lg col-span-2 h-full w-full"/>
