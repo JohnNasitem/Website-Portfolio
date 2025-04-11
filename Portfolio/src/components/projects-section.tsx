223e6d74fe771b598f04e1135551eae5382fd502
JohnNasitem <jnasitem1@nait.ca>
2025-04-10 20:26:27 -0600

    added cronocord link

--- Portfolio/src/components/projects-section.tsx
+++ Portfolio/src/components/projects-section.tsx
@@ -22,7 +22,7 @@ const ProjectsSection = () => {
                         <InfoCard link='/projects#Spool_Meter_Management_Sytem' title="Spool Meter Management Sytem" description="Keep track of the remaining amount of material left in a spool and predict when it will run out." image="/smms.png"/>
                     </div>
                     <div id='projects_section3' className={`${section3Visible ? 'animate-fade-slide-in' : ''}`}>
-                        <InfoCard link='/projects' title="Cronocord" description="Discord bot helps manage schedules betweeen multiple people." image="/discord_bot.png"/>
+                        <InfoCard link='/projects#Cronocord' title="Cronocord" description="Discord bot helps manage schedules betweeen multiple people." image="/discord_bot.png"/>
                     </div>
                     <div id='projects_section4' className={`${section4Visible ? 'animate-fade-slide-in' : ''}`}>
                         <InfoCard link='/projects' title="Website Porfolio" description="The site you are looking at right now." image="/site_page.png"/>
